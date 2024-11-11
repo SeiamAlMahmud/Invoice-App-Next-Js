d80dc90280d60281c36826865f6bbe5a3ef39162
SeiamAlMahmud <samahmud49@gmail.com>
2024-11-11 22:26:45 +0600

    first commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col justify-center h-hfull gap-6 text-center max-w-5xl mx-auto">
+      <h1 className="text-5xl font-bold">Dashboard</h1>
+      
+    </main>
+  );
+}
