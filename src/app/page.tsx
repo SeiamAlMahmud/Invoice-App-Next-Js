1ec28f95743237bee4f8cdeef5145cf8b3afbec9
SeiamAlMahmud <samahmud49@gmail.com>
2024-11-14 15:11:55 +0600

    implement clerk for authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs';
 import { Button } from "@/components/ui/button"
 import Link from "next/link";
 
@@ -5,12 +11,20 @@ export default function Home() {
   return (
     <main className="flex flex-col justify-center h-screen gap-6 text-center max-w-5xl mx-auto">
       <h1 className="text-5xl font-bold">Invoicepedia</h1>
-      <p>
-      <Button asChild>
+      <div>
+      <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+      </div>
+      <div>
+          <Button asChild>
         <Link href="/dashboard">
           Sign In</Link>
       </Button>
-      </p>
+      </div>
     </main>
   );
 }
